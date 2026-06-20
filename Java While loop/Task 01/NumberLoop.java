5868533b5debfacb9281e863bcbee4e550811e22
Gajanan <sagadevangajanan@gmail.com>
2026-06-20 22:17:30 +0530

    number loop

---
+++ Java While loop/Task 01/NumberLoop.java
@@ -0,0 +1,10 @@
+public class NumberLoop{
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number <= 100) {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
